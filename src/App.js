0abf2ec7c0a79f5be79535e86e124238bbf9b439
Vicky <vignesha0101@gmail.com>
2024-10-29 19:52:39 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter, Route, Routes} from 'react-router-dom';
 import { Navbar } from './component/Navbar';
 import { Home } from './pages/Home';
 import { EventInfo } from './pages/EventInfo';
@@ -11,18 +11,18 @@ function App () {
 
   
   return (
-    <BrowserRouter>
+    <HashRouter>
     <AuthProvider>
       <Navbar />      
       <Routes>
-        <Route path="//" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/eventinfo" element={<EventInfo/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Login/>} />
         <Route path="/ticket-booking" element={<TicketBooking />} />
       </Routes>
     </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
